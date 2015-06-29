6191549cda469b91929b7c38c7d3c7465745d636
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2015-06-29 13:42:46 +0700

    zzzz

--- 404.php
+++ 404.php
@@ -7,50 +7,41 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+  <!-- BREADCRUMB
+  ================================================== -->
+        
+  <?php get_template_part( 'template-parts/content', 'breadcrumb' ); ?>
+  
+  <!-- END BREADCRUMB
+  ================================================== -->
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'mmjvn-theme' ); ?></h1>
-				</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'mmjvn-theme' ); ?></p>
 
-					<?php get_search_form(); ?>
+  <!-- MAIN CONTENT
+  ================================================== -->
 
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+  <div class="polygon-bg polygon-bg--a padding-big">
+    
+    <div class="row">
+      <div class="small-12 medium-9 columns">
 
-					<?php if ( mmjvn_theme_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'mmjvn-theme' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-					<?php endif; ?>
+				<section class="error-404 not-found">
+					<header class="page-header">
+						<h1 class="page-title"><?php esc_html_e( 'Oops! Trang này không tìm thấy.', 'mmjvn-theme' ); ?></h1>
+					</header><!-- .page-header -->
+				</section><!-- .error-404 -->
 
-					<?php
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'mmjvn-theme' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
+      </div>
+      <div class="small-12 medium-3 columns">
+				<?php get_sidebar(); ?>
+      </div>
+    </div>
+    <!-- /.row -->
 
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+  </div>
+  <!-- /.polygon-bg -->
+  
+  <!-- END MAIN CONTENT
+  ================================================== -->
 
 <?php get_footer(); ?>
